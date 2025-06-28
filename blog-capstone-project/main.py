4fa5192943ae981f1e2adddd45a243fb18c9c09f
adityajain1812 <142815869+Adityajain1812@users.noreply.github.com>
2025-06-28 20:45:44 +0530

    Update main.py

--- blog-capstone-project/main.py
+++ blog-capstone-project/main.py
@@ -11,6 +11,11 @@ def get_all_posts():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
   
 
 if __name__ == "__main__":
